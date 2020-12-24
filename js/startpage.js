7adf3583b458b01dc2a4121f29e11fa02149a16d
John Hunter <hunterjw.usa@gmail.com>
2020-12-24 15:38:20 -0600

    Adding greeting messages

--- js/startpage.js
+++ js/startpage.js
@@ -8,7 +8,7 @@ var days = [
 	"Saturday"
 ];
 
-var months = [
+const months = [
 	"January",
 	"Febuary",
 	"March",
@@ -23,6 +23,62 @@ var months = [
 	"December"
 ];
 
+const lunchGreetings = [
+	"Enjoy your break",
+	"Lunch time!",
+	"Enjoy your lunch"
+];
+
+const morningGreetings = [
+	"Good Morning",
+	"Have a wonderful day",
+	"Enjoy your day",
+	"Thanks for watching"
+];
+
+const nightGreetings = [
+	"Sleep well",
+	"Good Night",
+	"Night night",
+	"Time for bed"
+];
+
+const afternoonGreetings = [
+	"Call it a day",
+	"Pack up work",
+	"Finish Move",
+	"Game Over"
+];
+
+function GetGreeting(hours) {
+	const random = a => a[Math.floor(Math.random() * a.length)];
+
+	var toReturn = "";
+	if (hours < 12) {
+		if (hours > 5) {
+			toReturn = random(morningGreetings);
+		}
+		else if (hours > 11) {
+			toReturn = random(lunchGreetings);
+		}
+		else {
+			toReturn = random(nightGreetings);
+		}
+	}
+	else {
+		if (hours < 13) {
+			toReturn = random(lunchGreetings);
+		}
+		if (hours > 21) {
+			toReturn = random(nightGreetings);
+		}
+		else {
+			toReturn = random(afternoonGreetings);
+		}
+	}
+	return toReturn;
+}
+
 function GetTimeString() {
 	var now = new Date();
 	var day = now.getDay();
@@ -32,18 +88,26 @@ function GetTimeString() {
 	var hour = now.getHours();
 	var minute = now.getMinutes();
 
+	var greetingString = GetGreeting(hour);
+
 	hour = (hour < 10 ? "0" : "") + hour;
 	minute = (minute < 10 ? "0" : "") + minute;
-
-	return hour + ":" + minute + " " + days[day] + " " + months[month]
+	var timeString = hour + ":" + minute + " " + days[day] + " " + months[month]
 		+ " " + date + " " + year;
+
+	return {
+		time: timeString,
+		greeting: greetingString
+	};
 }
 
 function Main() {
 	console.log("Hello world");
-	var timeString = GetTimeString();
-	console.log(timeString);
-	document.getElementById("time-text").textContent = timeString;
+	var strings = GetTimeString();
+	console.log(strings.time);
+	console.log(strings.greeting);
+	document.getElementById("time-text").textContent = strings.time;
+	document.getElementById("greeting-text").textContent = strings.greeting;
 }
 
 Main();
